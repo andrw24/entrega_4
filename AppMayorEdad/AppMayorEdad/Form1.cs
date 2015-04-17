78c2a3aad929743a6811d2d304db26a39dd91935
andrw24 <andreagils24@gmail.com>
2015-04-16 21:24:35 -0500

    entrega_4
    .NET

---
+++ AppMayorEdad/AppMayorEdad/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMayorEdad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Persona persona1 = new Persona(textBox1.Text,int.Parse(textBox2.Text));
+            if (persona1.Edad >= 18)
+            {
+                MessageBox.Show(persona1.Nombre + " es mayor de edad");
+            }
+            else
+            {
+                MessageBox.Show(persona1.Nombre + " es menor de edad");
+            }
+
+        }
+    }
+}
---
+++ AppVomboBox/AppVomboBox/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppVomboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int cont = 0; cont <= 255; cont++)
+            {
+                comboBox1.Items.Add(cont.ToString());
+                comboBox2.Items.Add(cont.ToString());
+                comboBox3.Items.Add(cont.ToString());
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rojo, verde, azul;
+            rojo= int.Parse(comboBox1.Text);
+            verde= int.Parse(comboBox2.Text);
+            azul = int.Parse(comboBox3.Text);
+            BackColor = Color.FromArgb(rojo,verde,azul);
+        }
+        
+        
+    }
+}
